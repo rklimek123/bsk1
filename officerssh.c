68072337bb198567467befc64c5d5046aebfb26b
Rafał Klimek <rafal1klimek@gmail.com>
2021-11-28 14:31:16 +0100

    Infiniloop

--- officerssh.c
+++ officerssh.c
@@ -1,16 +1,30 @@
 #include <security/pam_appl.h>
 #include <security/pam_misc.h>
 #include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
 
 static struct pam_conv login_conv = {
     misc_conv,
     NULL
 };
- 
-int main () {
-    pam_handle_t* loginh = NULL;
+
+pam_handle_t* loginh = NULL;
+
+void sigint_handler(int signum) {
+    pam_end(loginh, PAM_SUCCESS);
+    exit(0);
+}
+
+void workflow() {
+    
+}
+
+int main() {
     int retval;
     char *username = NULL;
+
+    signal(SIGINT, sigint_handler);
   
     retval = pam_start("officerapp", username, &login_conv, &loginh);
     if (loginh == NULL || retval != PAM_SUCCESS) {
@@ -24,7 +38,9 @@ int main () {
         exit(2);
     }
 
-    printf("Hello world!\n");
+    for (;;) {
+        workflow();
+    }
     
     pam_end(loginh, PAM_SUCCESS);
     return 0;
